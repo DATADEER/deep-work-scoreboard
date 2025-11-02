e2cdb12599aa6fd189abefa5bb83aeb3443c31f1
DATADEER <konto@datadeer.de>
2025-11-02 10:43:48 +0100

    run python command unbuffered

--- run-on-pi.sh
+++ run-on-pi.sh
@@ -5,6 +5,6 @@ LOG_FILE="$HOME/deep-work-scoreboard/cron.log"
 echo "=== $(date) ===" >> "$LOG_FILE"
 
 source "$HOME/.virtualenvs/pimoroni/bin/activate" && \
-python "$HOME/deep-work-scoreboard/main.py" --display | tee -a "$LOG_FILE"
+python -u "$HOME/deep-work-scoreboard/main.py" --display | tee -a "$LOG_FILE"
 
 echo "Exit code: $?" >> "$LOG_FILE"
